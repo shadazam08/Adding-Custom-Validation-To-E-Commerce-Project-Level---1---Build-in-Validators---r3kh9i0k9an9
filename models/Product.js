efa5bc633419099f821bd4407b7c3b0827b43566
Shad Azam <58129443+shadazam08@users.noreply.github.com>
2023-05-15 19:30:03 +0530

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -5,24 +5,35 @@ const mongoose = require('mongoose');
 
 /*
 Add Custom Validation to Product Model
-Validate that the product name is at least 3 characters long, the description is at least 10 characters long, and the price is a positive number.
-Throw an error is the Validation fails.
+Validate that the product name is at least 3 characters long, the description is at least 10 characters long, and the price is a positive number
 */
 const productSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
-        //Add a Custom Validation Here
+        validate(value) {
+            if (value.length < 3) {
+                throw new Error('Product name must be at least 3 characters long')
+            }
+        }
     },
     description: {
         type: String,
         required: true,
-        //Add a Custom Validation Here
+        validate(value){
+            if (value.length < 10) {
+                throw new Error('Description must be at least 10 characters long')
+            }
+        }
     },
     price: {
         type: Number,
         required: true,
-        //Add a Custom Validation Here
+        validate(value){
+            if (value < 0) {
+                throw new Error('Price must be a positive number')
+            }
+        }
     },
     category: {
         type: String,
